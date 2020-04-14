16bbf75f68f397378cc804b895a6ea62c7daf917
Karol Galazka <ugryzionybanan@gmail.com>
2020-04-14 13:29:00 +0200

    start zad2

---
+++ APBD_zad2/APBD_zad2/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace APBD_zad2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
